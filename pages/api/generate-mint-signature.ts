11a14303fd50f6c4186b83b7b6548368f618bd23
ayaztest <107302142+ayaztest@users.noreply.github.com>
2022-08-30 02:38:50 +0500

    Update generate-mint-signature.ts

--- pages/api/generate-mint-signature.ts
+++ pages/api/generate-mint-signature.ts
@@ -3,15 +3,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function generateMintSignature(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
+
 ) {
   // De-construct body from request
   const { address } = JSON.parse(req.body);
-
+const { quantity } = JSON.parse(req.body);
   // Get the Early Access NFT Edition Drop contract
   const polygonSDK = new ThirdwebSDK("polygon");
   const earlyAccessNfts = polygonSDK.getEditionDrop(
-    "0xa9e893cc12026a2f6bd826fdb295eac9c18a7e88"
+    "0x728a5887e13e34Ac61c554b0Ae45433C2A3ce5A6"
   );
 
   // Check to see if the wallet address has an early access NFT
@@ -28,26 +29,30 @@ export default async function generateMintSignature(
   }
 
   // Now use the SDK on Goerli to get the signature drop
-  const goerliSDK = ThirdwebSDK.fromPrivateKey(
+  const BinanceSmartChainMainnetSDK = ThirdwebSDK.fromPrivateKey(
     process.env.PRIVATE_KEY as string,
-    "goerli"
+    "binance"
   );
-  const signatureDrop = goerliSDK.getSignatureDrop(
-    "0xb90a18e9270d44F6e7D06e5Eac32C6Ea881CCaB2"
+  
+  const signatureDrop = BinanceSmartChainMainnetSDK.getSignatureDrop(
+    "0xE62d775E3Cc91659034dFC3b09a46259D6942c2c"
   );
 
   // If the user has an early access NFT, generate a mint signature
-  if (userHasToken) {
-    const mintSignature = await signatureDrop.signature.generate({
-      to: address, // Can only be minted by the address we checked earlier
-      price: "0", // Free!
-      mintStartTime: new Date(0), // now
-    });
-
+  if (userHasToken ) {
+   const mintSignature = await signatureDrop.signature.generate({
+     to: address, // Can only be minted by the address we checked earlier
+     quantity: quantity,
+     price: parseInt(quantity) < 3 ? "3" : "1", 
+    currencyAddress: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+    mintStartTime: new Date(0), // now
+  })
     res.status(200).json(mintSignature);
   } else {
     res.status(400).json({
       message: "User does not have an early access NFT",
     });
   }
-}
+} 
+
+
