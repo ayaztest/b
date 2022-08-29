3ccc94d48632fd2b1b1ad00952558bf5c8740464
ayaztest <107302142+ayaztest@users.noreply.github.com>
2022-08-30 02:39:17 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import ThirdwebGuideFooter from "../components/GitHubLink";
+
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Goerli;
+const activeChainId = ChainId.BinanceSmartChainMainnet;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Component {...pageProps} />
-      <ThirdwebGuideFooter />
+      
     </ThirdwebProvider>
   );
 }
